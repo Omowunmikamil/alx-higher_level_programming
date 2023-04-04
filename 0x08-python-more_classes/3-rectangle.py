33bc932bd28d61babc3246c5e778f55a199fb498
Omowunmijuin <balikiskamil@gmail.com>
2023-04-03 18:49:34 -0700

    String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -5,17 +5,13 @@ Defines a Rectangle
 
 
 class Rectangle:
-    """Represents a rectangle"""
-
-    def _init_(self, width=0, height=0):
-        """Initializes the current rectangle.
+    """Initializes the current rectangle.
          Args:
             height (int): height of the rectangle
             width (int): width of the rectangle
         """
-        self.width = width
-        self.height = height
-        def __init__(self, width=0, height=0):
+
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
