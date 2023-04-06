6933881f994474d89009f5506fa576fb055be4a8
Omowunmijuin <balikiskamil@gmail.com>
2023-04-06 05:52:10 -0700

    make changes

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,17 +11,18 @@ def text_indentation(text):
 
     if type(text) is not str:
         raise TypeError("text must be a string")
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
     
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
+    x = 0
+    while x < len(text) and text[x] == ' ':
+        x += 1
+    
+    while x < len(text):
+        print(text[x], end="")
+        if text[x] == "\n" or text[x] in ".?:":
+            if text[x] in ".?:":
                 print("\n")
-            c +=1
-            while c < len(text) and text[c] == ' ':
-                c += 1
-                continue
-        c += 1
+            x +=1
+            while x < len(text) and text[x] == ' ':
+                x += 1
+            continue
+        x += 1
