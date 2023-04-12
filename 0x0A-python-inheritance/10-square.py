4737d40ba0f1254bd3ee16f264bc9a33e66a89a2
Omowunmijuin <balikiskamil@gmail.com>
2023-04-11 22:24:13 -0700

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""efines a Rectangle subclass Square."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize the size of a new square.
+        Args:
+            size (int): private
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
