66d5e4212685fde266589f7a447e36d813604f20
Omowunmijuin <balikiskamil@gmail.com>
2023-04-12 19:56:22 -0700

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """
-script to save and load
+script to save and load all arguments in a list
 """
+
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
 filename = 'add_item.json'
-my_list = []
+
 try:
     my_list = load_from_json_file(filename)
-except Exception:
-    save_to_json_file(my_list, filename)
+except FileNotFoundError:
+    my_list = []
 
-arg_len = len(argv)
+for arg in argv[1:]:
+    my_list.append(arg)
 
-if arg_len > 1:
-    for i in range(1, arg_len):
-        my_list.append(argv[i])
-
-    save_to_json_file(my_list, filename)
+save_to_json_file(my_list, filename)
