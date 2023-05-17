07b8eec473895e02db502ff9362ad08e68561819
Omowunmijuin <balikiskamil@gmail.com>
2023-05-17 09:57:38 -0700

    Cheating is bad

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- The below script updates the sc ore of Bob to 10 in the table second_table.
-UPDATE second_table set score = 10 WHERE "bob";
+UPDATE second_table SET score = 10 WHERE name = "Bob";
