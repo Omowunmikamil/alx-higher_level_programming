049e229d3b1b4b9f039d3099629196d5850d4624
Omowunmijuin <balikiskamil@gmail.com>
2023-06-15 18:24:02 +0000

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Argument that creates sentence with JavaScript
 
-console.log(process.argv[2] + 'is' + process.argv[3]);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
