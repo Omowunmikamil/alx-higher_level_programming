3714eea2ad574dc3cf44066eaa5f5a7ef42e5bdf
Omowunmijuin <balikiskamil@gmail.com>
2023-06-15 15:37:37 +0000

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-// Basic JavaScript that prints 3 lines.
+// Basic print with Javascript
 
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
