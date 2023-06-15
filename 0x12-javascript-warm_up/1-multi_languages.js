2de16d2b85013366920d5a26591d95445c3eff50
Omowunmijuin <balikiskamil@gmail.com>
2023-06-15 15:41:03 +0000

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // Basic print with Javascript
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
