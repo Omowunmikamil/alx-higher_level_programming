1c4ba74283239d037ec8cf21bf8f3a8af016a24b
Omowunmijuin <balikiskamil@gmail.com>
2023-06-16 01:06:08 +0000

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Print basic JavaScript
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amaizing'];
+
+let index = 0;
+while (array[index]) {
+  console.log(array[index]);
+  index++;
+}
