5533593cf36dc811e672f93bb3c31a1deb31a02f
Omowunmijuin <balikiskamil@gmail.com>
2023-06-16 01:46:35 +0000

    Add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// addintion with basic JavaScript
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+function add (a, b) {
+  console.log(a + b);
+}
+add(a, b);
