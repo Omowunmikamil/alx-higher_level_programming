ae3ca7b5aa24df9469a244d6154750516e6d8558
Omowunmijuin <balikiskamil@gmail.com>
2023-06-16 02:10:38 +0000

    Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// JavaScript factorial
+
+let firstVar = process.argv.slice(2);
+const secondVar = process.argv.length;
+
+if (secondVar <= 3) {
+  console.log('0');
+} else {
+  firstVar = firstVar.map(Number);
+  const thirVar = firstVar.sort(
+    function (x, y) {
+      return y - x;
+    })[1];
+  console.log(thirVar);
+}
